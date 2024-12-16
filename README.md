263708e56a88a059b50d422fea28c58aed08eea2
Tetsuya Mito <tetsuya@wardish.jp>
2024-12-16 11:50:49 +0900

    INITIAL VERSION

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # powerbi-single-datepicker
 Single Datepicker for PowerBI
+
+WSL2 でビルドすることを想定した構成となっています。  
+2024-12-16の実装ですが、各種ライブラリに合わせてTypeScriptは古いバージョンを利用しています.
+
+## 構成
+
+`pbiviz`をnpxで実行するため、二段階のフォルダ構成を取っています.  
+直下で`npm i`することでpbivizがインストールされます.
+
+その後、`cd singleDatepicker`して`npm i`してください.
+
+
+## 参照
+
+オフィシャルサンプル  
+https://github.com/microsoft/PowerBI-visuals-sampleBarChart
+
+ブログ（2020年の記事なので現行とは差異があるが、基本的な概念はそのまま）  
+https://qiita.com/kenakamu/items/f57ab7f73d6ea10bc9d9
---
+++ singleDatepicker/src/visual.ts
@@ -0,0 +1,154 @@
+/*
+*  Power BI Visual CLI
+*
+*  Copyright (c) Microsoft Corporation
+*  All rights reserved.
+*  MIT License
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the ""Software""), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in
+*  all copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*  THE SOFTWARE.
+*/
+"use strict";
+
+import { parseISO, format, subDays } from 'date-fns';
+import powerbi from "powerbi-visuals-api";
+import FilterAction = powerbi.FilterAction;
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
+import {
+    IAdvancedFilter,
+} from "powerbi-models";
+import { FormattingSettingsService } from "powerbi-visuals-utils-formattingmodel";
+import "./../style/visual.less";
+
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+
+import { VisualFormattingSettingsModel } from "./settings";
+
+/**
+ * ビジュアルの本体
+ */
+export class Visual implements IVisual {
+    private target: HTMLElement;
+    private formattingSettings: VisualFormattingSettingsModel;
+    private formattingSettingsService: FormattingSettingsService;
+
+    private visualHost: IVisualHost;
+
+    private datepickerText: HTMLInputElement;
+
+    private targetTableName: string;
+    private targetColumnName: string;
+
+    private prevCondition: string;
+
+    constructor(options: VisualConstructorOptions) {
+        this.visualHost = options.host;
+        this.formattingSettingsService = new FormattingSettingsService();
+        this.target = options.element;
+
+        if (document) {
+            this.datepickerText = document.createElement('input');
+            // ブラウザの機能に基づいた日付ピッカーが表示されることを期待する。
+            this.datepickerText.type = "date";
+            // ロケールに基づいた日付文字列が得られる
+            this.datepickerText.value = format(subDays(new Date(), 1), "yyyy-MM-dd");
+            this.target.appendChild(this.datepickerText);
+
+            this.datepickerText.addEventListener("change", (e) => {
+                const ev = e.target as HTMLInputElement
+                this.applyFilter(ev.value);
+            });
+            this.applyFilter(this.datepickerText.value);
+        }
+    }
+
+    /**
+     * 他ビジュアルの更新時フック
+     * 
+     * @param options VisualUpdateOptions
+     */
+    public update(options: VisualUpdateOptions) {
+        this.formattingSettings = this.formattingSettingsService.populateFormattingSettingsModel(VisualFormattingSettingsModel, options.dataViews[0]);
+
+        // single なので決め打ち
+        if (options.dataViews.length > 0 && options.dataViews[0].metadata.columns.length > 0) {
+            const metadata = options.dataViews[0].metadata.columns[0];
+            // テーブル名.列名 の形式になっていることを前提とする。
+            this.targetTableName = metadata.queryName?.split('.')[0];
+            this.targetColumnName = metadata.displayName;
+            this.applyFilter(this.datepickerText.value);
+        }
+    }
+
+    /**
+     * Returns properties pane formatting model content hierarchies, properties and latest formatting values, Then populate properties pane.
+     * This method is called once every time we open properties pane or when the user edit any format property. 
+     */
+    public getFormattingModel(): powerbi.visuals.FormattingModel {
+        return this.formattingSettingsService.buildFormattingModel(this.formattingSettings);
+    }
+
+    /**
+     * applyFilter
+     * 
+     * 指定した日付で対象データを絞り込み
+     * 00:00:00 - 23:59:59
+     * 
+     * @param selectedDate picked date
+     * @returns void
+     */
+    public applyFilter(selectedDate: string): void {
+        // 同一条件で複数回クエリが飛ぶのを抑制
+        if (this.prevCondition === selectedDate) return;
+
+        // 前提条件が整っていない場合はクエリを行わない
+        if (!this.visualHost || !this.targetTableName || !this.targetColumnName) return;
+
+        let conditions = [];
+        // 日付入力があるときだけ検索
+        if (selectedDate || selectedDate !== '') {
+            // 日付時刻型を決め打ち
+            conditions = [
+                {
+                    operator: "GreaterThanOrEqual",
+                    value: parseISO(`${selectedDate}T00:00:00+09:00`).toISOString()
+                },
+                {
+                    operator: "LessThanOrEqual",
+                    value: parseISO(`${selectedDate}T23:59:59.999+09:00`).toISOString()
+                }
+            ]
+        }
+
+        // 指定した日付範囲の全ての時刻をFrom-Toで含める
+        let filter: IAdvancedFilter = {
+            $schema: "https://powerbi.com/product/schema#advanced",
+            target: {
+                table: this.targetTableName,
+                column: this.targetColumnName
+            },
+            conditions,
+            logicalOperator: "And",
+            filterType: 0
+        }
+        this.visualHost.applyJsonFilter(filter, "general", "filter", FilterAction.merge);
+        this.prevCondition = selectedDate;
+    }
+}
